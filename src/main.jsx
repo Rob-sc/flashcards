352dca46438347e4dcbba083284c62cc1f639caf
Rob_scr <roberto_scr@outlook.com>
2024-02-01 05:15:36 +0100

    update

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,19 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-    <h1>Holi mundo</h1>
-  </React.StrictMode>,
+
+const root = ReactDOM.createRoot(document.getElementById('root'))
+
+
+const Saludo = ({text}) => {
+  return (
+    <h1> {text} </h1>
+  )
+}
+
+
+root.render(
+<React.Fragment>
+  <Saludo  text="Hola Mundo"/>
+</React.Fragment>
 )
